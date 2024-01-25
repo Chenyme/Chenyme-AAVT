f50d5a35bd38853acc58fa97c3b32f74e678bcaf
Chenyme <118253778+Chenyme@users.noreply.github.com>
2024-01-25 23:32:35 +0800

    Delete cache/use.py

--- cache/use.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def srt_mv():
-    command = ' ffmpeg -i "' + "uploaded.mp4" + '" -lavfi ' + '"subtitles=' + 'output.srt' + ':force_style=' + "'BorderStyle=0,Outline=1,Shadow=0,Fontsize=18'" + '"' + ' -y -crf 1 -c:a copy "' + "output.mp4" + '"'
-    subprocess.call(command, shell=True)
